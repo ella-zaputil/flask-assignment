2807267805bfec18a18fa0c864a51f69b1fac86c
Ella Zaputil <ella-zaputil@uiowa.edu>
2025-10-30 12:15:55 -0500

    Created about page and added Bootstrap styling

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def home():
 
 @app.route("/about")
 def about():
-    return "This is Ella's about page."
+    return render_template("about.html")
 
 if __name__ == "__main__":
       app.run(host="0.0.0.0", port=5002, debug=True)
